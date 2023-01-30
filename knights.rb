ccbbfe023d93c349e5923e31fc6051605e141eb5
chridkee <christopher.keesor@gmil.com>
2023-01-30 14:50:42 -0500

    phase 1 finished for real ..probably

--- knights.rb
+++ knights.rb
@@ -1,27 +1,43 @@
 require_relative "./polytree.rb"
 
+#no more .children 
+
+
 class KnightPathFinder
-    attr_reader :root_node, :considered_positions
+    attr_reader :root_node, :considered_positions, :movements
+   @@movements = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1,-2], [-1, 2], [-1, -1]]
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
         build_move_tree(root_node)
         @considered_positions = [pos]
-        
     end
 
     def build_move_tree(root_node)
+        positions = []
         nodes = [root_node]
         until nodes.empty?
             node = nodes.shift
+            positions << node.value  
             nodes.concat(node.children)
+        end 
+        return positions 
     end 
-
-    def self.valid_moves(root_node)
-        root_node.children 
-    end 
+    def self.valid_moves(root_node) 
+        pos = root_node.value
+        moves = @@movements.map do |el|
+            [pos[0] + el[0], pos[1] + el[1]] 
+        end
+        moves.select! do |indices|
+            (indices[0] < 8 && indices[0] >= 0 ) && (indices[1] < 8 && indices[1] >= 0) 
+        end
+        moves
+    end
+  
 
     def new_move_positions(pos)
-        @considered_positions.concat(KnightPathFinder.valid_moves(pos).select {|ele| !@considered_positions.include?(ele)})
+        new_moves = (KnightPathFinder.valid_moves(pos).select {|ele| !@considered_positions.include?(ele)})
+        @considered_positions.concat(new_moves)
+        return new_moves
     end 
 
     def inspect
@@ -41,9 +57,16 @@ pos_1.parent = kpf.root_node
 pos_2.parent = kpf.root_node
 
 #  kpf.root_node
- KnightPathFinder.valid_moves(kpf.root_node)
-# p kpf.new_move_positions(kpf.root_node)
-# p kpf.considered_positions.each do |ele|
-#     p ele
+p kpf.new_move_positions(kpf.root_node)
+p kpf.considered_positions
+
 # end  
 
+# kpf.build_move_tree(kpf.root_node, kpf.)
+
+# p KnightPathFinder.valid_moves(kpf.root_node)
+
+#(2, 2)
+
+
+ 
